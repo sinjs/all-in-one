c5a42be0cd4db35c61678b7d76f14005cd70277c
sindotexe <sinmineryt@github.com>
2021-03-18 22:19:19 +0100

    Add rickroll because I don't have anything better to do

--- src/Routes.js
+++ src/Routes.js
@@ -12,6 +12,8 @@ class Routes extends React.Component {
                     {/* Benis */}
                 </Route>
 
+                <Route path="/contact-the-owner" exact component={() => {window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"; return <>pleese wait</>}} />
+
                 <Route path="*">
                     <NotFound />
                 </Route>
