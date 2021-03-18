49bcb9ef8e3b5ab179977671ba93211279ab9fbf
sindotexe <sinmineryt@github.com>
2021-03-18 22:19:47 +0100

    Add 404 page with rickroll

--- src/Components/Pages/NotFound.js
+++ src/Components/Pages/NotFound.js
@@ -1,8 +1,34 @@
 import React from 'react';
 
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import '../../Style/NotFound.css'
+
 class NotFound extends React.Component {
     render() {
-        return <></>;
+        return <>
+            <Container maxWidth="sm" align="center">
+                <Box my={3}>
+                    <Typography variant="h3">
+                        404
+                    </Typography>
+                    <Box my={2}>
+                        <Typography variant="h5">
+                            Page not found
+                        </Typography>
+                    </Box>
+                    <Box my={2}>
+                        <Typography variant="body1">
+                            We couldn't find the requested page on the server. <br />
+                            If you think this is a server error, plaese contact <a href="/contact-the-owner">the owner</a>.
+                        </Typography>
+                    </Box>
+                </Box>
+
+            </Container>
+        </>;
     }
 }
 
